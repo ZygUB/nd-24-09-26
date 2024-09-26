e69b6e868db26b284d41075b0c83fcc9e97eb8a5
ZygUB <zygvlogs1@gmail.com>
2024-09-26 19:24:12 +0300

    commiting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [post, setPost] = useState({});
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        const json = await response.json();
+        setPost(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchPost();
+  }, []);
+
+  useEffect(() => {
+    if (post.userId) {
+      const fetchUser = async () => {
+        try {
+          const response = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`);
+          const json = await response.json();
+          setUser(json);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+
+      fetchUser();
+    }
+  }, [post.userId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {user ? (
+        <div className='Info'>
+          <p><strong>{user.name}</strong></p>
+          <p>{user.email}</p>
+          <p>{user.company?.name}</p>
+        </div>
+      ) : (
+        <p>Loading user data...</p>
+      )}
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
     </div>
   );
 }
